afca2345ccaefb00e4113d52e67404cd6b341345
JAM <ahujordan@yahoo.com>
2022-10-20 00:22:42 +0100

    edited info to suit client

--- src/components/Projects/ProjectLinks.js
+++ src/components/Projects/ProjectLinks.js
@@ -5,55 +5,60 @@ import {BsLink45Deg} from "react-icons/bs";
 import Card from "react-bootstrap/Card";
 import "./projectLinks.css";
 import image_1 from "assets/images/evans-device.jpg";
-import image_2 from "assets/images/evans-device2.jpg";
 import image_3 from "assets/images/evans-work.jpg";
+import image_2 from "assets/images/evans-device2.jpg";
+import image_4 from "assets/images/evans-device3.jpg";
 
 export default function ProjectLinks({projectLinks}) {
-  const [open, setOpen] = useState(false);
-  const images = [image_1, image_2, image_3];
+  // const [open, setOpen] = useState(false);
+  const images = [image_1, image_4, image_2, image_3];
 
   return (
     <>
-      <Button
-        className="color--border mb-5"
-        onClick={() => setOpen(!open)}
-        aria-controls="example-collapse-text"
-        aria-expanded={open}
-      >
-        <BsLink45Deg /> | Links Of Projects
-      </Button>
-      <Collapse in={open}>
-        <div id="example-collapse-text">
-          <div className="d-flex flex-row flex-wrap">
-            {projectLinks.map(({ summary, title }, index) => (
-              <div key={index}>
-                <Card
-                  style={{ width: "20rem" }}
-                  className="color-body my-2 mx-2 bg-dark"
-                >
-                  <Card.Img
-                    variant="top"
-                    style={{ objectFit: "contain" }}
-                    width="200rem"
-                    height="200rem"
-                    src={images[index]}
-                  />
-                  <Card.Body>
-                    <Card.Title>{title}</Card.Title>
-                    <Card.Text>{summary}</Card.Text>
-                    {/* <Card.Link
+      <h3 className="color-body text-center mt-5 color--link">
+        LIST OF PROJECTS
+      </h3>
+      <div className="d-flex flex-row flex-wrap mt-5">
+        {projectLinks.map(({ summary, title }, index) => (
+          <div key={index}>
+            <Card
+              style={{ width: "20rem" }}
+              className="color-body my-2 mx-2 bg-dark"
+            >
+              <Card.Img
+                variant="top"
+                style={{ objectFit: "contain" }}
+                width="200rem"
+                height="200rem"
+                src={images[index]}
+              />
+              <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>{summary}</Card.Text>
+                {/* <Card.Link
                       className="color--link"
                       href="https://github.com"
                     >
                       Github Repo
                     </Card.Link> */}
-                  </Card.Body>
-                </Card>
-              </div>
-            ))}
+              </Card.Body>
+            </Card>
           </div>
+        ))}
+      </div>
+      {/* <Button
+        className="color--border mb-5"
+        onClick={() => setOpen(!open)}
+        aria-controls="example-collapse-text"
+        aria-expanded={open}
+      >
+        <BsLink45Deg /> | List Of Projects
+      </Button>
+      <Collapse in={open}>
+        <div id="example-collapse-text">
+         
         </div>
-      </Collapse>
+      </Collapse> */}
     </>
   );
 }
